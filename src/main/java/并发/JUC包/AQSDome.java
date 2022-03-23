e209447c09da172db7f797192c408061b9c31f88
wanfeng <804121985@qq.com>
2022-03-23 21:35:56 +0800

    reentrantLock学习!

--- src/main/java/并发/JUC包/AQSDome.java
+++ src/main/java/并发/JUC包/AQSDome.java
@@ -40,6 +40,8 @@ public class AQSDome {
             }
         }).start();
     }
+
+    ReentrantLock reentrantLock = new ReentrantLock();
 }
 
 
---
+++ src/main/java/并发/JUC包/ReentrantReadWriteDome.java
@@ -0,0 +1,21 @@
+package 并发.JUC包;
+
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * @author wanfeng
+ * @create 2022/3/23 20:53
+ * @package 并发.JUC包
+ */
+public class ReentrantReadWriteDome {
+
+    private ReentrantReadWriteLock  rw =  new ReentrantReadWriteLock();
+
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
